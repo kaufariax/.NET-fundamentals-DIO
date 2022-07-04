eb40faaf62c43d62938275a1c10fd53f5eba08bf
kaufariax <eikauanefarias@gmail.com>
2022-07-03 23:27:12 -0300

    @kaufariax [Task: Refatoração de Código] {03/07/2022}

--- Mandioca/Program.cs
+++ Mandioca/Program.cs
@@ -2,16 +2,22 @@
 
 class Mandioca {
   static void Main() {
+    
     Console.WriteLine("Digite quantas porções vai querer Chico: ");
     var chico = 300 * Int32.Parse(Console.ReadLine());
+
     Console.WriteLine("Digite quantas porções vai querer Bento: ");
     var bento = 1500 * Int32.Parse(Console.ReadLine());
+
     Console.WriteLine("Digite quantas porções vai querer Bernardo: ");
     var bernardo = 600 * Int32.Parse(Console.ReadLine());
+
     Console.WriteLine("Digite quantas porções vai querer Marina: ");
     var marina = 1000 * Int32.Parse(Console.ReadLine());
+
     Console.WriteLine("Digite quantas porções vai querer Iara: ");
     var iara = 150 * Int32.Parse(Console.ReadLine());
+
     var marlene = 225;
     var total = chico + bento + bernardo + marina + iara + marlene;
     
