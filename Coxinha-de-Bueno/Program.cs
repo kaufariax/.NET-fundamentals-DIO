83eee8ae6e213f88d8bf9e5bb4f4c4e0923ff743
kaufariax <eikauanefarias@gmail.com>
2022-07-04 15:49:37 -0300

    @kaufariax [Task: Desafio Coxinha de Bueno] {04/07/2022}

---
+++ Coxinha-de-Bueno/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class CoxinhaDeBueno {
+  static void Main() {
+    Console.WriteLine("Insira a quantidade de coxinhas e participantes no seguinte formato: 'coxinhas participantes'");
+    string[] line = Console.ReadLine().Split(" ");
+    double coxinhas = int.Parse(line[0]);
+    double participantes = int.Parse(line[1]);
+    double media = coxinhas / participantes;
+    
+    Console.WriteLine(media.ToString("N1"));
+  }
+}
