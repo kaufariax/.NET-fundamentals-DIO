951baf8213598e01479f2866f6ef904cfc305299
kaufariax <eikauanefarias@gmail.com>
2022-07-03 23:02:49 -0300

    @kaufariax [Task: Desafio de Distância] {03/07/2022}

---
+++ Distancia/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Distancia {
+  static void Main() {
+    Console.WriteLine("Insira os quilômetros de distância: ");
+    int quilometros = Int32.Parse(Console.ReadLine());
+    int minutos = quilometros * 2;
+    
+    Console.WriteLine("levará " + minutos + " minutos para a moto Y tomar essa distância da outra moto ");
+  }
+}
