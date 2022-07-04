e01edbccc5d9a347e7b9169207c3f47571af532d
kaufariax <eikauanefarias@gmail.com>
2022-07-03 23:22:58 -0300

    @kaufariax [Task: Desafio 'Quanta Mandioca?'] {03/07/2022}

---
+++ Mandioca/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Mandioca {
+  static void Main() {
+    Console.WriteLine("Digite quantas porções vai querer Chico: ");
+    var chico = 300 * Int32.Parse(Console.ReadLine());
+    Console.WriteLine("Digite quantas porções vai querer Bento: ");
+    var bento = 1500 * Int32.Parse(Console.ReadLine());
+    Console.WriteLine("Digite quantas porções vai querer Bernardo: ");
+    var bernardo = 600 * Int32.Parse(Console.ReadLine());
+    Console.WriteLine("Digite quantas porções vai querer Marina: ");
+    var marina = 1000 * Int32.Parse(Console.ReadLine());
+    Console.WriteLine("Digite quantas porções vai querer Iara: ");
+    var iara = 150 * Int32.Parse(Console.ReadLine());
+    var marlene = 225;
+    var total = chico + bento + bernardo + marina + iara + marlene;
+    
+    Console.WriteLine("Marlene, você vai precisar preparar " + total + " gramas de mandioca!");
+  }
+}
